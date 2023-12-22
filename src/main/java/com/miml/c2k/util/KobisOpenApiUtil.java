4c32f721544cdcc06e99544495f30d816058ffdc
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2023-12-23 01:00:28 +0900

    feat #2: Kobis Open Api를 이용한 영화 등록 기능 구현
    - 영화 코드로 영화 상세 내역을 불러와 저장
    - 기존에 등록된 영화인 경우에는 저장하지 않도록 함

---
+++ src/main/java/com/miml/c2k/util/KobisOpenApiUtil.java
@@ -0,0 +1,104 @@
+package com.miml.c2k.util;
+
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.miml.c2k.domain.movie.Movie;
+import com.miml.c2k.domain.movie.repository.MovieRepository;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.Optional;
+import org.apache.http.HttpResponse;
+import org.apache.http.client.ClientProtocolException;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.utils.URIBuilder;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
+
+@Component
+public class KobisOpenApiUtil {
+
+    private final MovieRepository movieRepository;
+    private final CloseableHttpClient httpClient;
+    @Value("${spring.kobis.url.base}")
+    private String baseUrl;
+    @Value("${spring.kobis.url.movie-info}")
+    private String movieInfoUrl;
+    @Value("${spring.kobis.key}")
+    private String key;
+
+    public KobisOpenApiUtil(MovieRepository movieRepository) {
+        this.movieRepository = movieRepository;
+        httpClient = HttpClientBuilder.create().build();
+    }
+
+    public Movie getMovieInfoByMovieCodeAndSave(String movieCode) {
+        String movieInfoBaseUrl = baseUrl + movieInfoUrl;
+        Optional<Movie> movie = movieRepository.findByCode(Long.valueOf(movieCode));
+        if (movie.isPresent()) {
+            return movie.get();
+        } else {
+            try {
+                // HTTP GET 요청 생성
+                URI requestMovieInfoUri = new URIBuilder(movieInfoBaseUrl)
+                        .addParameter("key", key)
+                        .addParameter("movieCd", movieCode)
+                        .build();
+
+                // 요청 전송 및 응답 수신
+                JsonNode rootNode = executeHttpRequestAndGetJsonResponse(requestMovieInfoUri);
+
+                // 데이터 필터링
+                Long code;
+                String title, genre, director, nation;
+
+                JsonNode movieInfoJsonNode = rootNode.path("movieInfoResult").path("movieInfo");
+                code = movieInfoJsonNode.path("movieCd").asLong();
+                title = movieInfoJsonNode.path("movieNm").asText();
+                JsonNode directorsNode = movieInfoJsonNode.path("directors");
+                director =
+                        directorsNode.size() >= 1 ? directorsNode.get(0).path("peopleNm").asText()
+                                : null;
+                JsonNode genresNode = movieInfoJsonNode.path("genres");
+                genre = genresNode.size() >= 1 ? genresNode.get(0).path("genreNm").asText() : null;
+                JsonNode nationsNode = movieInfoJsonNode.path("nations");
+                nation = nationsNode.size() >= 1 ? nationsNode.get(0).path("nationNm").asText()
+                        : null;
+                // TODO: 포스터 별도 크롤링 필요
+
+                return movieRepository.save(new Movie(title, director, genre, nation, code));
+            } catch (URISyntaxException e) {
+                e.printStackTrace();
+            } catch (ClientProtocolException e) {
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+
+        throw new RuntimeException();
+    }
+
+    private JsonNode executeHttpRequestAndGetJsonResponse(URI uri) throws IOException {
+        HttpGet httpGet = new HttpGet(uri);
+        HttpResponse response = httpClient.execute(httpGet);
+        BufferedReader reader = new BufferedReader(
+                new InputStreamReader(response.getEntity().getContent()));
+        StringBuilder result = new StringBuilder();
+        String line;
+        while ((line = reader.readLine()) != null) {
+            result.append(line);
+        }
+        ObjectMapper objectMapper = new ObjectMapper();
+        JsonNode rootNode = objectMapper.readTree(result.toString());
+
+        return rootNode;
+    }
+}
