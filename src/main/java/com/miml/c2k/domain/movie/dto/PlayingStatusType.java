d29737aa3964532320616e264765c77dfad3569c
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2023-12-23 00:44:35 +0900

    feat #2: 영화 상영 상태를 나타내는 이넘 클래스 추가

---
+++ src/main/java/com/miml/c2k/domain/movie/dto/PlayingStatusType.java
@@ -0,0 +1,24 @@
+package com.miml.c2k.domain.movie.dto;
+
+public enum PlayingStatusType {
+
+    IS_PLAYING(1, "상영 중"), WILL_PLAYING(2, "상영 예정");
+
+    private final Integer num;
+    private final String name;
+
+    PlayingStatusType(Integer num, String name) {
+        this.num = num;
+        this.name = name;
+    }
+
+    public static PlayingStatusType getValueByNum(int num) {
+        for (PlayingStatusType playingStatusType : PlayingStatusType.values()) {
+            if (playingStatusType.num == num) {
+                return playingStatusType;
+            }
+        }
+
+        throw new IllegalArgumentException("예외 처리 필요");
+    }
+}
