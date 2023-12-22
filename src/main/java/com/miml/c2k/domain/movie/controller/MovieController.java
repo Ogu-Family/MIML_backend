f136900d11b2acc888be8299acc52819b46d4d40
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2023-12-23 01:14:55 +0900

    feat #2: 상영중인 영화 리스트 반환 API 구현

--- src/main/java/com/miml/c2k/domain/movie/controller/MovieController.java
+++ src/main/java/com/miml/c2k/domain/movie/controller/MovieController.java
@@ -1,6 +1,8 @@
 package com.miml.c2k.domain.movie.controller;
 
+import com.miml.c2k.domain.movie.dto.MovieResponseDto;
 import com.miml.c2k.domain.movie.service.MovieService;
+import com.miml.c2k.domain.movie.dto.PlayingStatusType;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -12,6 +14,12 @@ import org.springframework.web.bind.annotation.RestController;
 public class MovieController {
 
     private final MovieService movieService;
+
+    @GetMapping("/api/v1/movies")
+    public List<MovieResponseDto> getMoviesPlayingOnScreen(
+            @RequestParam("playing_status") int num) {
+        return movieService.getMoviesPlayingOnScreen(PlayingStatusType.getValueByNum(num));
+    }
 
     @GetMapping("/api/v1/movies/top10")
     public void saveTop10Movie(@RequestParam("target-date") String targetDate) {
