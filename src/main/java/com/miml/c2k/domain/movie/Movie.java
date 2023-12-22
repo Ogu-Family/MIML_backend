6520fbfc95f8b4f6058c8c3ccec6a8c51c85638a
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2023-12-23 00:43:59 +0900

    feat #2: 영화 엔터티 수정
    - 장르 및 국적 자료형 문자열로 변경(오픈 API에서 그대로 가져오기 위함.)
    - 영화 코드 속성 추가(오픈 API 사용 시 필요)

--- src/main/java/com/miml/c2k/domain/movie/Movie.java
+++ src/main/java/com/miml/c2k/domain/movie/Movie.java
@@ -2,14 +2,17 @@ package com.miml.c2k.domain.movie;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
-import jakarta.persistence.EnumType;
-import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
 
 @Entity
+@NoArgsConstructor
+@Getter
 public class Movie {
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
@@ -20,17 +23,19 @@ public class Movie {
     @Column(name = "director")
     private String director;
 
-    @Enumerated(EnumType.STRING)
     @Column(name = "genre")
-    private Genre genre;
+    private String genre;
 
-    @Enumerated(EnumType.STRING)
     @Column(name = "nation")
-    private Nation nation;
+    private String nation;
 
     @Column(name = "poster", columnDefinition = "text")
-	private String poster;
+    private String poster;
 
     @Column(name = "audience_count", nullable = false, columnDefinition = "int default 0")
-    private Integer audienceCount;
+    private Long audienceCount = 0L;
+
+    @Column(name = "code", nullable = false, unique = true)
+    private Long code;
+
 }
