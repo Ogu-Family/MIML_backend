515aa797f985690979a9e694fcda972d3091a99f
ZZAMBAs <rksidksrksi@naver.com>
2023-12-24 16:36:30 +0900

    feat #6: 영화관, 상영관 기본적인 Repository 추가

---
+++ src/main/java/com/miml/c2k/domain/screen/repository/ScreenRepository.java
@@ -0,0 +1,8 @@
+package com.miml.c2k.domain.screen.repository;
+
+import com.miml.c2k.domain.screen.Screen;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ScreenRepository extends JpaRepository<Screen, Long> {
+
+}
