4a68c904b472af77c0b84848e6fcb68888e68375
ZZAMBAs <rksidksrksi@naver.com>
2023-12-25 03:59:21 +0900

    fix: 필드 대입 수정

--- src/main/java/com/miml/c2k/domain/theater/dto/TheaterAdminResponseDto.java
+++ src/main/java/com/miml/c2k/domain/theater/dto/TheaterAdminResponseDto.java
@@ -1,6 +1,5 @@
 package com.miml.c2k.domain.theater.dto;
 
-import com.miml.c2k.domain.screen.Screen;
 import com.miml.c2k.domain.theater.Theater;
 
 public class TheaterAdminResponseDto {
@@ -10,7 +9,7 @@ public class TheaterAdminResponseDto {
 
     private TheaterAdminResponseDto(Long id, String name) {
         this.id = id;
-
+        this.name = name;
     }
 
     public static TheaterAdminResponseDto create(Theater theater) {
