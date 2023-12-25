80b7929a5c6e57977a6dd6480fff9c41f956c036
kimnamgyu <lethargic.hood@gmail.com>
2023-12-25 12:04:26 +0900

    feat #2: 카카오 로그인 api 관련 설정 구현

---
+++ src/main/java/com/miml/c2k/global/auth/platform/kakao/KakaoLoginParams.java
@@ -0,0 +1,27 @@
+package com.miml.c2k.global.auth.platform.kakao;
+
+import com.miml.c2k.domain.member.OAuthProvider;
+import com.miml.c2k.global.auth.platform.OAuthLoginParams;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import org.springframework.util.LinkedMultiValueMap;
+import org.springframework.util.MultiValueMap;
+
+@Getter
+@NoArgsConstructor
+public class KakaoLoginParams implements OAuthLoginParams {
+
+    private String authorizationCode;
+
+    @Override
+    public OAuthProvider oAuthProvider() {
+        return OAuthProvider.KAKAO;
+    }
+
+    @Override
+    public MultiValueMap<String, String> makeBody() {
+        MultiValueMap<String, String> body = new LinkedMultiValueMap<>();
+        body.add("code", authorizationCode);
+        return body;
+    }
+}
