ae92ce722f6ab8161e07bfcb7df85c40d5097e05
ZZAMBAs <rksidksrksi@naver.com>
2023-12-28 21:24:41 +0900

    feat: 사용자가 선택한 조건대로 상영 일정을 가져오는 API 추가

--- src/main/java/com/miml/c2k/domain/schedule/controller/ScheduleController.java
+++ src/main/java/com/miml/c2k/domain/schedule/controller/ScheduleController.java
@@ -1,17 +1,35 @@
 package com.miml.c2k.domain.schedule.controller;
 
+import com.miml.c2k.domain.schedule.dto.ScheduleViewResponseDto;
 import com.miml.c2k.domain.schedule.service.ScheduleService;
+import com.miml.c2k.global.dto.ApiResponse;
+import java.time.LocalDate;
+import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.apache.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequiredArgsConstructor
 public class ScheduleController {
     private final ScheduleService scheduleService;
 
-    @GetMapping("/schedule")
+    @GetMapping("/schedules")
     public String showSchedulePage() {
         return "schedule/select-schedule";
+    }
+
+    @GetMapping("/api/v1/schedules")
+    @ResponseBody
+    public ApiResponse<List<ScheduleViewResponseDto>> getSchedulesBy(@RequestParam Long movieId,
+        @RequestParam Long theaterId,
+        @RequestParam LocalDate date) {
+        List<ScheduleViewResponseDto> scheduleViewResponseDtos = scheduleService.getSchedulesBy(movieId,
+            theaterId, date);
+
+        return ApiResponse.create(HttpStatus.SC_OK, "정상 처리", scheduleViewResponseDtos);
     }
 }
