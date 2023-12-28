bbf49de3bbf0d1bcad24ecb76e2954299296a94d
ZZAMBAs <rksidksrksi@naver.com>
2023-12-28 21:21:08 +0900

    feat: 영화, 영화관, 상영 일자에 따라 상영 일정을 보여주는 쿼리 추가

--- src/main/java/com/miml/c2k/domain/schedule/repository/ScheduleRepository.java
+++ src/main/java/com/miml/c2k/domain/schedule/repository/ScheduleRepository.java
@@ -2,6 +2,7 @@ package com.miml.c2k.domain.schedule.repository;
 
 import com.miml.c2k.domain.movie.Movie;
 import com.miml.c2k.domain.schedule.Schedule;
+import java.time.LocalDate;
 import java.time.LocalDateTime;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
@@ -17,4 +18,9 @@ public interface ScheduleRepository extends JpaRepository<Schedule, Long> {
     @Query("SELECT count(s) FROM Schedule s WHERE (s.screen.id = :screenId) AND ((s.startTime BETWEEN :startTime AND :endTime) OR (s.endTime BETWEEN :startTime AND :endTime))")
     int countAllByScreenIdBetweenTimeline(Long screenId, LocalDateTime startTime,
             LocalDateTime endTime);
+
+    @Query(value = "SELECT sche FROM Schedule sche "
+        + "JOIN FETCH Screen sc ON sc.id = sche.screen.id "
+        + "WHERE sc.theater.id = :theaterId AND Date(sche.startTime) = :date AND sche.movie.id = :movieId")
+    List<Schedule> findAllByMovieIdAndTheaterIdAndDate(Long movieId, Long theaterId, LocalDate date);
 }
