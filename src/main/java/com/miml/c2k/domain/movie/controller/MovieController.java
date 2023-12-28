365e48bcd73e394ea97c2a1e4435bad3301db273
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2023-12-28 22:52:35 +0900

    feat #8: 상영 상태에 따른 영화 목록 반환 API 추가 구현

--- src/main/java/com/miml/c2k/domain/movie/controller/MovieController.java
+++ src/main/java/com/miml/c2k/domain/movie/controller/MovieController.java
@@ -5,25 +5,30 @@ import com.miml.c2k.domain.movie.dto.PlayingStatusType;
 import com.miml.c2k.domain.movie.service.MovieService;
 import java.util.List;
 import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
 
-@RestController
+@Controller
 @RequiredArgsConstructor
 public class MovieController {
 
     private final MovieService movieService;
 
     @GetMapping("/api/v1/movies")
-    public List<MovieResponseDto> getMoviesPlayingOnScreen(
-            @RequestParam("playing_status") int num) {
-        return movieService.getMoviesPlayingOnScreen(PlayingStatusType.getValueByNum(num));
-    }
+    public String getMoviesByPlayingStatus(@RequestParam("playing_status") int num, Model model) {
+        List<MovieResponseDto> movies;
+        PlayingStatusType playingStatus = PlayingStatusType.getValueByNum(num);
+        switch (playingStatus) {
+            case IS_PLAYING -> movies = movieService.getMoviesPlayingOnScreen();
+            case WILL_PLAYING -> movies = movieService.getMoviesWillPlaying();
+            default -> throw new RuntimeException("유효하지 않은 상영 상태 값 입니다.");
+        }
+        model.addAttribute("movies", movies);
+        model.addAttribute("playingStatusTitle", playingStatus.getName());
 
-    @GetMapping("/api/v1/movies/top10")
-    public void saveTop10Movie(@RequestParam("target-date") String targetDate) {
-        movieService.saveBoxOfficeTop10ByTargetDate(targetDate);
+        return "member/movie-list";
     }
 
 }
