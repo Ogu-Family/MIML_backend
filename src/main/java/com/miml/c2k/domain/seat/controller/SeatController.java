8b21f9371acd83dc4337e8ad33cbe3ce5621a203
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2024-01-01 23:57:31 +0900

    feat #14: 선택된 좌석 예약 API 구현

--- src/main/java/com/miml/c2k/domain/seat/controller/SeatController.java
+++ src/main/java/com/miml/c2k/domain/seat/controller/SeatController.java
@@ -1,12 +1,18 @@
 package com.miml.c2k.domain.seat.controller;
 
+import com.miml.c2k.domain.seat.dto.ReservedSeatResponseDto;
 import com.miml.c2k.domain.seat.dto.SeatRequestDto;
 import com.miml.c2k.domain.seat.service.SeatService;
+import com.miml.c2k.global.dto.ApiResponse;
 import lombok.RequiredArgsConstructor;
+import org.apache.http.HttpStatus;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequiredArgsConstructor
@@ -19,5 +25,13 @@ public class SeatController {
             @RequestParam("schedule_id") Long scheduleId, Model model) {
         model.addAttribute("seats", seatService.getAllSeats(scheduleId));
         return "schedule/select-seat";
+    }
+
+    @PostMapping("/api/v1/seats")
+    @ResponseBody
+    public ApiResponse<ReservedSeatResponseDto> getSchedulesBy(
+            @RequestBody SeatRequestDto seatRequestDto) {
+        return ApiResponse.create(HttpStatus.SC_OK, "정상 처리",
+                seatService.reserveSeat(seatRequestDto));
     }
 }
