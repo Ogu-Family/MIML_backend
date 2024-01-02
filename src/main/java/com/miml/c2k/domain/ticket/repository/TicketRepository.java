f3008e7a31b03de50a85e1630cf01054253c8b78
ZZAMBAs <rksidksrksi@naver.com>
2024-01-03 00:40:12 +0900

    feat: 회원 id와 연관된 티켓을 조회하는 쿼리 추가

--- src/main/java/com/miml/c2k/domain/ticket/repository/TicketRepository.java
+++ src/main/java/com/miml/c2k/domain/ticket/repository/TicketRepository.java
@@ -1,8 +1,17 @@
 package com.miml.c2k.domain.ticket.repository;
 
+import com.miml.c2k.domain.schedule.Schedule;
 import com.miml.c2k.domain.ticket.Ticket;
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
 
+    @Query(value = "SELECT t FROM Ticket t WHERE t.member.id = :memberId")
+    List<Ticket> findAllByMemberId(Long memberId);
+
+    @Query(value = "SELECT s FROM Schedule s JOIN Ticket t ON s.id = t.schedule.id WHERE t.id = :ticketId")
+    Optional<Schedule> findScheduleByTicketId(Long ticketId);
 }
