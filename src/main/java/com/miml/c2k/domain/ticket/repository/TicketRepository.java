863ea51a65c880c176f1b5766877bfc4593b8eaa
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2024-01-02 20:40:32 +0900

    feat #14: Repository 어노테이션 삭제

--- src/main/java/com/miml/c2k/domain/ticket/repository/TicketRepository.java
+++ src/main/java/com/miml/c2k/domain/ticket/repository/TicketRepository.java
@@ -2,9 +2,7 @@ package com.miml.c2k.domain.ticket.repository;
 
 import com.miml.c2k.domain.ticket.Ticket;
 import org.springframework.data.jpa.repository.JpaRepository;
-import org.springframework.stereotype.Repository;
 
-@Repository
 public interface TicketRepository extends JpaRepository<Ticket, Long> {
 
 }
