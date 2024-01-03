b04fe0db10d161ea2aa7e5d4d928cc27fb55eb83
kimnamgyu <lethargic.hood@gmail.com>
2024-01-04 05:21:44 +0900

    feat #19: role 필드 추가

--- src/main/java/com/miml/c2k/domain/member/dto/MemberResponseDto.java
+++ src/main/java/com/miml/c2k/domain/member/dto/MemberResponseDto.java
@@ -1,6 +1,7 @@
 package com.miml.c2k.domain.member.dto;
 
 import com.miml.c2k.domain.member.OAuthProvider;
+import com.miml.c2k.domain.member.Role;
 import java.time.LocalDateTime;
 import lombok.Builder;
 import lombok.Getter;
@@ -11,13 +12,15 @@ public class MemberResponseDto {
     private final String nickname;
     private final String email;
     private final OAuthProvider oAuthProvider;
+    private final Role role;
     private final LocalDateTime createdAt;
 
     @Builder
-    public MemberResponseDto(String nickname, String email, OAuthProvider oAuthProvider, LocalDateTime createdAt) {
+    public MemberResponseDto(String nickname, String email, OAuthProvider oAuthProvider, Role role, LocalDateTime createdAt) {
         this.nickname = nickname;
         this.email = email;
         this.oAuthProvider = oAuthProvider;
+        this.role = role;
         this.createdAt = createdAt;
     }
 
