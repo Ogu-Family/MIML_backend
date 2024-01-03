0b057db6ab6e3fd9bc3ee974f5e1873b1a9fb520
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2024-01-03 22:33:01 +0900

    feat #20: 여러 좌석 선택에 따른 요청, 반환 dto 수정

--- src/main/java/com/miml/c2k/domain/seat/dto/SeatRequestDto.java
+++ src/main/java/com/miml/c2k/domain/seat/dto/SeatRequestDto.java
@@ -1,16 +1,17 @@
 package com.miml.c2k.domain.seat.dto;
 
 import com.miml.c2k.domain.seat.Seat.SeatNameType;
+import java.util.List;
 import lombok.Getter;
 
 @Getter
 public class SeatRequestDto {
 
     private final Long scheduleId;
-    private final SeatNameType seatNameType;
+    private final List<SeatNameType> seatNameTypes;
 
-    public SeatRequestDto(Long scheduleId, SeatNameType seatNameType) {
+    public SeatRequestDto(Long scheduleId, List<SeatNameType> seatNameTypes) {
         this.scheduleId = scheduleId;
-        this.seatNameType = seatNameType;
+        this.seatNameTypes = seatNameTypes;
     }
 }
