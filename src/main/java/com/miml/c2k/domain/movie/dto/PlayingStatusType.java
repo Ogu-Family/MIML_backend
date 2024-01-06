88907acbdd5f9c24af302a848d75a3c599e7e79d
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2024-01-06 21:24:11 +0900

    feat #26: 영화 상영 상태 값 변환에 대한 예외 처리 추가

--- src/main/java/com/miml/c2k/domain/movie/dto/PlayingStatusType.java
+++ src/main/java/com/miml/c2k/domain/movie/dto/PlayingStatusType.java
@@ -2,24 +2,19 @@ package com.miml.c2k.domain.movie.dto;
 
 public enum PlayingStatusType {
 
-    IS_PLAYING(1, "상영 중"), WILL_PLAYING(2, "상영 예정");
-
-    private final Integer num;
+    IS_PLAYING("상영 중"), WILL_PLAYING("상영 예정");
     private final String name;
 
-    PlayingStatusType(Integer num, String name) {
-        this.num = num;
+    PlayingStatusType(String name) {
         this.name = name;
     }
 
-    public static PlayingStatusType getValueByNum(int num) {
-        for (PlayingStatusType playingStatusType : PlayingStatusType.values()) {
-            if (playingStatusType.num == num) {
-                return playingStatusType;
-            }
+    public static PlayingStatusType getValue(String playingStatus) {
+        try {
+            return PlayingStatusType.valueOf(playingStatus.toUpperCase());
+        } catch (IllegalArgumentException e) {
+            throw new IllegalArgumentException("유효하지 않은 상영 상태 값 입니다.");
         }
-
-        throw new IllegalArgumentException("예외 처리 필요");
     }
 
     public String getName() {
