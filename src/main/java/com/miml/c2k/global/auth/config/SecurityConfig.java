5c76570ca0d20cb1af6d48c1656c63e7cb5cb282
kimnamgyu <lethargic.hood@gmail.com>
2024-01-07 15:51:23 +0900

    rename #27: Config 패키지 이동

--- src/main/java/com/miml/c2k/global/auth/config/SecurityConfig.java
+++ src/main/java/com/miml/c2k/global/auth/config/SecurityConfig.java
@@ -1,11 +1,10 @@
-package com.miml.c2k.global.auth;
+package com.miml.c2k.global.auth.config;
 
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
 import org.springframework.security.web.SecurityFilterChain;
 
 @EnableWebSecurity
