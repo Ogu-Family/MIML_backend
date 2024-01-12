1d2f250444d9a08112726579f60f22a4fcaaf28d
JeongeunChoi <77786996+JeongeunChoi@users.noreply.github.com>
2024-01-12 13:52:40 +0900

    test #40: 멤버 id로 티켓 정보 fetch join 하는 jpql 테스트 코드 추가

--- src/test/java/com/miml/c2k/domain/ticket/repository/TicketRepositoryTest.java
+++ src/test/java/com/miml/c2k/domain/ticket/repository/TicketRepositoryTest.java
@@ -1,18 +1,29 @@
 package com.miml.c2k.domain.ticket.repository;
 
+import static com.miml.c2k.domain.DataFactoryUtil.createMember;
+import static com.miml.c2k.domain.DataFactoryUtil.createMoviesIsPlaying;
+import static com.miml.c2k.domain.DataFactoryUtil.createSchedules;
+import static com.miml.c2k.domain.DataFactoryUtil.createScreens;
+import static com.miml.c2k.domain.DataFactoryUtil.createTheaters;
 import static org.assertj.core.api.Assertions.assertThat;
 
 import com.miml.c2k.domain.member.Member;
 import com.miml.c2k.domain.member.repository.MemberRepository;
+import com.miml.c2k.domain.movie.repository.MovieRepository;
 import com.miml.c2k.domain.schedule.Schedule;
 import com.miml.c2k.domain.schedule.repository.ScheduleRepository;
+import com.miml.c2k.domain.screen.Screen;
+import com.miml.c2k.domain.screen.repository.ScreenRepository;
 import com.miml.c2k.domain.seat.Seat;
 import com.miml.c2k.domain.seat.Seat.SeatNameType;
 import com.miml.c2k.domain.seat.repository.SeatRepository;
+import com.miml.c2k.domain.theater.repository.TheaterRepository;
 import com.miml.c2k.domain.ticket.Ticket;
 import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
 import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -30,6 +41,12 @@ class TicketRepositoryTest {
     private SeatRepository seatRepository;
     @Autowired
     private ScheduleRepository scheduleRepository;
+    @Autowired
+    private ScreenRepository screenRepository;
+    @Autowired
+    private TheaterRepository theaterRepository;
+    @Autowired
+    private MovieRepository movieRepository;
 
     @Test
     @DisplayName("member id로 특정 회원의 티켓들을 가져온다.")
@@ -56,18 +73,53 @@ class TicketRepositoryTest {
     }
 
     @Test
+    @DisplayName("member id로 특정 회원의 티켓들을 fetch join 하여 가져온다.")
+    void success_find_all_tickets_with_fetch_join_by_member_id_() {
+        // given
+        int ticketCnt = 3;
+        Screen screen = screenRepository.saveAll(
+                createScreens(theaterRepository.saveAll(createTheaters()))).get(0);
+        List<Schedule> schedules = scheduleRepository.saveAll(
+                createSchedules(
+                        movieRepository.saveAll(createMoviesIsPlaying(ticketCnt)), screen));
+        List<SeatNameType> seatNameTypesToReserve = Stream.of(SeatNameType.J11, SeatNameType.J12,
+                SeatNameType.J13).toList();
+        Member member = memberRepository.save(createMember());
+        schedules.forEach(schedule -> {
+            Ticket ticket = ticketRepository.save(
+                    Ticket.builder().member(member).schedule(schedule).build());
+            List<Seat> seats = seatNameTypesToReserve.stream()
+                    .map(seatNameType -> Seat.builder()
+                            .name(seatNameType)
+                            .screen(schedule.getScreen())
+                            .ticket(ticket)
+                            .build())
+                    .collect(Collectors.toList());
+            seatRepository.saveAll(seats);
+            seats.forEach(ticket::addSeat);
+        });
+
+        // when
+        List<Ticket> tickets = ticketRepository.findAllByMemberIdWithFetchJoin(member.getId());
+
+        // then
+        assertThat(tickets).hasSize(ticketCnt);
+    }
+
+    @Test
     @DisplayName("특정 티켓과 연결된 상영 일정을 가져온다.")
     void success_find_a_schedule_by_ticket_id() {
         // given
         Schedule schedule = Schedule.builder().startTime(LocalDateTime.now())
-            .endTime(LocalDateTime.now()).build();
+                .endTime(LocalDateTime.now()).build();
         Ticket ticket = Ticket.builder().schedule(schedule).build();
 
         scheduleRepository.save(schedule);
         ticketRepository.save(ticket);
 
         // when
-        Optional<Schedule> retrievedSchedule = ticketRepository.findScheduleByTicketId(ticket.getId());
+        Optional<Schedule> retrievedSchedule = ticketRepository.findScheduleByTicketId(
+                ticket.getId());
 
         // then
         assertThat(retrievedSchedule.isPresent()).isTrue();
@@ -75,7 +127,8 @@ class TicketRepositoryTest {
     }
 
     @Test
-    @Disabled // TODO: 결제 구현 후 작성
+    @Disabled
+        // TODO: 결제 구현 후 작성
     void success_find_payment_by_ticket_id() {
     }
 }
