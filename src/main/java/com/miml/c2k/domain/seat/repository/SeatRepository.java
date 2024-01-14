c21a0d4ac67fb185770233bc50403fd31e6759ec
ZZAMBAs <rksidksrksi@naver.com>
2024-01-14 18:17:56 +0900

    feat #41: Schedule Id와 좌석 이름 리스트를 받아 해당하는 좌석들을 전부 가져오는 쿼리 추가

--- src/main/java/com/miml/c2k/domain/seat/repository/SeatRepository.java
+++ src/main/java/com/miml/c2k/domain/seat/repository/SeatRepository.java
@@ -1,8 +1,12 @@
 package com.miml.c2k.domain.seat.repository;
 
 import com.miml.c2k.domain.seat.Seat;
+import com.miml.c2k.domain.seat.Seat.SeatNameType;
+import jakarta.persistence.LockModeType;
+import java.util.Collection;
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Lock;
 import org.springframework.data.jpa.repository.Query;
 
 public interface SeatRepository extends JpaRepository<Seat, Long> {
@@ -10,6 +14,9 @@ public interface SeatRepository extends JpaRepository<Seat, Long> {
     @Query(value = "SELECT count(se) FROM Seat se JOIN Ticket t ON se.ticket.id = t.id WHERE t.schedule.id = :scheduleId")
     int countReservedSeatsByScheduleId(Long scheduleId);
 
-    @Query("SELECT se FROM Seat se JOIN Ticket t ON se.ticket.id = t.id WHERE t.schedule.id = :scheduleId")
+    @Query("SELECT se FROM Seat se WHERE se.schedule.id = :scheduleId AND se.ticket IS NOT NULL")
     List<Seat> findAllReservedSeatsByScheduleId(Long scheduleId);
+
+    @Lock(LockModeType.PESSIMISTIC_WRITE)
+    List<Seat> findAllByScheduleIdAndNameIn(Long scheduleId, Collection<SeatNameType> names);
 }
